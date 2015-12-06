17097b4007d9f9baf0032f92c3074bd63c4087f4
Hend <cathe.2007@gmail.com>
2015-12-06 10:25:11 +0200

    Adding custom Preference View with custom Attribute

---
+++ app/src/main/java/com/hendalqett/app/sunshine/LocationEditTextPreference.java
@@ -0,0 +1,29 @@
+package com.hendalqett.app.sunshine;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+/**
+ * Created by hend on 12/6/15.
+ */
+public class LocationEditTextPreference extends EditTextPreference {
+    static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int mMinLength;
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.LocationEditTextPreference,
+                0, 0);
+        try {
+            mMinLength = a.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MINIMUM_LOCATION_LENGTH);
+        } finally {
+            a.recycle();
+        }
+    }
+
+
+}
